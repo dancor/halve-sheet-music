9f1907fd7b9a4718a9267cd5e5c1b36cc7de9e9d
Daniel Corson <danl@alum.mit.edu>
2014-10-29 14:27:24 -0400

    fix a couple -Wall

--- src/png-halve-sheet-music.hs
+++ src/png-halve-sheet-music.hs
@@ -3,6 +3,7 @@ import qualified Data.ByteString as BS
 import Data.Maybe
 import System.Environment
 
+isLeftWhiteHLine :: Image PixelRGB8 -> Int -> Int -> Bool
 isLeftWhiteHLine img leftW y = all (== PixelRGB8 255 255 255)
     [pixelAt img x y | x <- [0 .. leftW - 1]]
 
@@ -13,11 +14,11 @@ main = do
     let Right (ImageRGB8 img) = decodePng c
         imgH = imageHeight img
         imgHHalf = imgH `quot` 2
-        pred = isLeftWhiteHLine img
-        Just hMid = listToMaybe . filter (pred 43) $ concat
+        myPred = isLeftWhiteHLine img
+        Just hMid = listToMaybe . filter (myPred 43) $ concat
             [[h, imgH - h] | h <- [imgHHalf, imgHHalf - 1  .. 1]]
-        h1 = last $ takeWhile (pred 43) [hMid .. imgH - 1]
-        h2 = last $ takeWhile (pred 43) [hMid, hMid - 1 .. 0]
+        h1 = last $ takeWhile (myPred 43) [hMid .. imgH - 1]
+        h2 = last $ takeWhile (myPred 43) [hMid, hMid - 1 .. 0]
     {-
     print $ isLeftWhiteHLine img hMid 50
     print $ hMid
